ccbc2bcf34a0e46b07f1e9fb040626f8671fe90a
Niklas Strand <nikstra@users.noreply.github.com>
2021-11-17 20:56:46 +0100

    Inject base url.

--- Web/ClientApp/src/app/services/dynamic-data.service.ts
+++ Web/ClientApp/src/app/services/dynamic-data.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry, share } from 'rxjs/operators';
 import { ITableData } from '../components/dynamic-table/dynamic-table.component';
@@ -9,6 +9,8 @@ import { ITableData } from '../components/dynamic-table/dynamic-table.component'
 })
 export class DynamicDataService {
   
+  constructor(private httpClient: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+
   get(skip: number, take: number): Observable<ITableData> {
 
     const params = new HttpParams({
@@ -18,18 +20,16 @@ export class DynamicDataService {
       }
     });
     
-    const url = "https://localhost:5001/api/dynamicdata";
+    const url = this.baseUrl + "api/dynamicdata";
     return (this.httpClient.get(url, { params: params })
       .pipe(
         //retry(3),
         share(), // https://blog.novanet.no/angular-pitfall-multiple-http-requests-with-rxjs-and-observable-async/
         catchError((error: HttpErrorResponse) => {
 
-        if(error.status === 0) console.log("An error occured: ", error.error);
-        else console.error(`Backend returned code ${error.status}, body was: `, error.error);
-        return throwError("Something bad happened; please try again later.");
-    })) as Observable<ITableData>);
+          if(error.status === 0) console.log("An error occured: ", error.error);
+          else console.error(`Backend returned code ${error.status}, body was: `, error.error);
+          return throwError("Something bad happened; please try again later.");
+        })) as Observable<ITableData>);
   }
-
-  constructor(private httpClient: HttpClient) { }
 }
