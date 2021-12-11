f2566d11000a26b712738b15d78a46947f641091
Niklas Strand <nikstra@users.noreply.github.com>
2021-12-11 20:35:44 +0100

    Add Data project.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase("DemoDb");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
